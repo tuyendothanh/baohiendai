e483e9cc9d9e55be1815fb469e95e923a20e4a21
Tuyen Dothanh <tuyendothanhdng@gmail.com>
2017-06-15 18:14:27 +0900

    change api

--- app/src/main/java/com/manuelmaly/hn/util/HNHelper.java
+++ app/src/main/java/com/manuelmaly/hn/util/HNHelper.java
@@ -5,9 +5,9 @@ public class HNHelper {
     public static String resolveRelativeHNURL(String url) {
         if (url == null)
             return null;
-        
+
         String hnurl = "https://news.ycombinator.com/";
-        
+
         if (url.startsWith("http") || url.startsWith("ftp")) {
             return url;
         } else if (url.startsWith("/"))
